3f4380b1b26942c4e6e5ad55698b447783c043fa
jin <thseokjin@naver.com>
2022-11-10 10:26:51 +0900

    Create entity

---
+++ icia_library/src/main/java/com/iciaproject/icia_library/entity/Book.java
@@ -0,0 +1,36 @@
+package com.iciaproject.icia_library.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "book")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int b_id;
+
+    @Column(nullable = false, length = 20)
+    private String b_name;
+
+    @Column(nullable = false, length = 10)
+    private String b_author;
+
+    @Column
+    @CreationTimestamp
+    private Timestamp b_sdate;
+
+    @Column
+    @CreationTimestamp
+    private Timestamp b_edate;
+
+    @ColumnDefault("0")
+    @Column(nullable = false)
+    private Boolean b_lent;
+
+}
---
+++ icia_library/src/main/java/com/iciaproject/icia_library/entity/Member.java
@@ -0,0 +1,32 @@
+package com.iciaproject.icia_library.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "member")
+@Data
+public class Member {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String m_id;
+
+    @Column(nullable = false, length = 30)
+    private String m_pwd;
+
+    @Column(nullable = false, length = 20)
+    private String m_name;
+
+    @Column(nullable = false, length = 20)
+    private String m_phone;
+
+    @Column
+    @CreationTimestamp
+    private Timestamp m_birth;
+}
