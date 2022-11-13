9158943e8f330ecde8495493d5f95c84828b856f
kaeh42@naver.com <kaeh42@naver.com>
2022-11-14 08:52:31 +0900

    보드수정

--- icia_library/src/main/java/com/iciaproject/icia_library/entity/BoardFile.java
+++ icia_library/src/main/java/com/iciaproject/icia_library/entity/BoardFile.java
@@ -1,4 +1,24 @@
 package com.iciaproject.icia_library.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "boardfiletbl")
+@Data
 public class BoardFile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bfnum;
+
+    @ManyToOne
+    @JoinColumn(name = "bfbid")
+    private Board bfbid;
+
+    @Column(nullable = false, length = 30)
+    private String bfsysname;
+
+    @Column(nullable = false, length = 30)
+    private String bforiname;
 }
