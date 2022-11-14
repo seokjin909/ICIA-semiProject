77fd04f7348164abc9bd48716869bf7e472523ef
KChanHyeok <kang9kang9@gmail.com>
2022-11-14 11:18:46 +0900

    정리

--- icia_library/src/main/java/com/iciaproject/icia_library/repository/BoardFileRepository.java
+++ icia_library/src/main/java/com/iciaproject/icia_library/repository/BoardFileRepository.java
@@ -8,9 +8,5 @@
 //import java.util.List;
 //
 //public interface BoardFileRepository extends CrudRepository<BoardFile, Long> {
-<<<<<<< HEAD
 //    List<BoardFile> findByBfbid(Board board);
-=======
-////    List<BoardFile> findByBfbid(Board board);
->>>>>>> 7ab599445999e37fcbf1766f1fcb7049da62886c
-//}
+
